27f05cdb78f8141211999162bd250fe20356edbf
LeviJunior12 <levicoc123@gmail.com>
2020-10-12 10:53:49 -0300

    stable version 1.1

--- app/src/main/java/com/example/aulalayout/MainActivity.kt
+++ app/src/main/java/com/example/aulalayout/MainActivity.kt
@@ -3,45 +3,100 @@ package com.example.aulalayout
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.util.Log
+import android.view.KeyEvent
 import android.view.View
+import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatDelegate
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var imageSet: ImageView
+    lateinit var btnCalc: Button
+    lateinit var editTextGas: EditText
+    lateinit var editTextEthanol: EditText
+    private var textGas: Float = 0f
+    private var textEthanol: Float = 0f
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        AppCompatDelegate.setCompatVectorFromResourcesEnabled(true);
 
-        val editTextGas = findViewById<EditText>(R.id.editTextGas).text
-        val editTextEthanol = findViewById<EditText>(R.id. editTextEthanol).text
-        val btnCalc = findViewById<Button>(R.id.calc_btn)
-        val imageSet = findViewById<ImageView>(R.id.imageResult)
+        editTextGas = findViewById(R.id.editTextGas)
+        editTextEthanol = findViewById(R.id.editTextEthanol)
+        btnCalc = findViewById(R.id.calc_btn)
+        imageSet = findViewById(R.id.imageResult)
 
         btnCalc.setOnClickListener(View.OnClickListener {
-            if(editTextGas.isEmpty() || editTextEthanol.isEmpty()) {
-                getView().hideKeyboard()
-                Toast.makeText(this, "Por favor Complete Os Campos Acima!", Toast.LENGTH_LONG).show()
-            } else {
-                getView().hideKeyboard()
-                setImage(editTextGas.toString(), editTextEthanol.toString(), imageSet)
-            }
-
+            calcResult()
         })
-    }
 
-    private fun setImage(gas: String, ethanol: String, image: ImageView) {
-        if(bestGas(gas, ethanol)) {
-            image.setImageResource(R.drawable.abasteca_etanol)
-        } else {
-            image.setImageResource(R.drawable.abasteca_gasolina)
+        editTextEthanol.setOnEditorActionListener { _, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                calcResult()
+                true
+            }
+            false
         }
     }
 
-    private fun bestGas(gas: String, ethanol: String): Boolean {
-        return (ethanol.toFloat() / gas.toFloat()) <  0.7
+    private fun calcResult() {
+        if (checkField()) {
+            getView().hideKeyboard()
+            cleanField()
+            Toast.makeText(this, getString(R.string.error_field), Toast.LENGTH_LONG)
+                .show()
+        } else {
+            getView().hideKeyboard()
+            setImage()
+            textGas = 0f
+            textEthanol = 0f
+        }
+    }
+
+    private fun cleanField() {
+        editTextGas.text.clear()
+        editTextEthanol.text.clear()
+        imageSet.setImageResource(R.drawable.empty_dice)
+    }
+
+    private fun checkField(): Boolean {
+        if(!checkFieldBlank()) {
+            textGas = editTextGas.text.toString().toFloat()
+            textEthanol = editTextEthanol.text.toString().toFloat()
+        }
+        return checkFieldNumber()
+    }
+
+    private fun checkFieldBlank(): Boolean {
+        return editTextGas.text.toString().isBlank() || editTextEthanol.text.toString().isBlank()
+    }
+
+    private fun checkFieldNumber(): Boolean {
+        return textGas < 1 && textEthanol < 1
+    }
+
+    private fun setImage() {
+        when {
+            bestGas() -> {
+                imageSet.setImageResource(R.drawable.abasteca_etanol)
+            }
+            else -> {
+                imageSet.setImageResource(R.drawable.abasteca_gasolina)
+            }
+        }
+    }
+
+    private fun bestGas(): Boolean {
+        return (textEthanol / textGas) < 0.7
     }
 
     private fun View.hideKeyboard() {
@@ -50,7 +105,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun getView(): View {
-        return window.decorView.findViewById<View>(android.R.id.content)
+        return window.decorView.findViewById(android.R.id.content)
     }
 
 }
